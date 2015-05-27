b1a7f1d873574f94f3cb61e0923f33fa4f059163
Andrew McNutt <mcnutt.andrew@gmail.com>
2015-05-27 11:39:17 -0700

    Import of old plugin and jshint error correction

--- test/fixedheaderrewrite_test.js
+++ test/fixedheaderrewrite_test.js
@@ -2,9 +2,17 @@
   module('jQuery.fixedHeaderRewrite');
 
   test('is fixedHeaderRewrite', function () {
-    expect(2);
-    strictEqual($.fixedHeaderRewrite(), 'fixedHeaderRewrite.', 'should be fixedHeaderRewrite');
-    strictEqual($.fixedHeaderRewrite({punctuation: '!'}), 'fixedHeaderRewrite!', 'should be thoroughly fixedHeaderRewrite');
+    // expect(2);
+    ok(!!(_$.fn.fixedHeaderRewrite && _$.fn.fixedHeaderRewrite.constructor && _$.fn.fixedHeaderRewrite.call && _$.fn.fixedHeaderRewrite.apply));
+    // strictEqual(_$.fn.fixedHeaderRewrite, 'fixedHeaderRewrite.', 'should be fixedHeaderRewrite');
+    // strictEqual(_$.fn.fixedHeaderRewrite({punctuation: '!'}), 'fixedHeaderRewrite!', 'should be thoroughly fixedHeaderRewrite');
+  });
+  
+  test('given no arguments, header is sticky', function(){
+    _$('#testTable').fixedHeaderRewrite({
+      // fixedColumn: true
+    });
+    
   });
   
 }(jQuery));
